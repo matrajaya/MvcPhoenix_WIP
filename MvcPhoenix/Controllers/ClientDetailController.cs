b31163fa680f6c7f79db1ccac73bea306fedfaab
1feanyi <ifeanyi.igbo@gmail.com>
2015-10-08 17:19:29 -0400

    few changes and additions

---
+++ MvcPhoenix/Controllers/ClientDetailController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPhoenix.Controllers
+{
+    public class ClientDetailController : Controller
+    {
+        //
+        // GET: /ClientDetail/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
